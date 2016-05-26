7e7c65c1eac2e4557083ce0b1e97e74cc6d32a6e
Ethan Fetsko <ethanfetsko@Infrastructures-MacBook-Pro-3.local>
2016-05-26 13:38:30 -0400

    Altered the Map class' play method so that it now randomizes the order of the rooms.

--- map.rb
+++ map.rb
@@ -1,12 +1,14 @@
 class Map
   def initialize
-    @rooms = [ SkeletonRoom, SilverfishRoom, TreasureRoom, WispRoom, OgreRoom, ChapelRoom, ChickenRoom ]
+    @rooms = [ SkeletonRoom, SilverfishRoom, TreasureRoom, WispRoom, OgreRoom, ChapelRoom ]
   end
 
   def play
+    @rooms = @rooms.shuffle
     @rooms.each do |room|
       room.new.play
       Game.clear_screen
     end
+    ChickenRoom.new.play 
   end
 end
